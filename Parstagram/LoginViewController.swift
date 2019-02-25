3a28758bc6d7e27dfbb0be10fd183ef966e2cd01
Charlotte Hu <chanyueh@uci.edu>
2019-02-25 15:54:21 -0800

    Part 2

--- Parstagram/LoginViewController.swift
+++ Parstagram/LoginViewController.swift
@@ -7,16 +7,64 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var signinButton: UIButton!
+    
+    @IBOutlet weak var signupButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        signinButton.layer.cornerRadius = 5
+        signinButton.layer.borderWidth = 2
+        signinButton.titleEdgeInsets = UIEdgeInsets(top: 5,left: 5,bottom: 5,right: 5)
+        signinButton.layer.borderColor = UIColor.black.cgColor
+        signupButton.layer.cornerRadius = 5
+        signupButton.layer.borderWidth = 2
+        signupButton.titleEdgeInsets = UIEdgeInsets(top: 5,left: 5,bottom: 5,right: 5)
+        signupButton.layer.borderColor = UIColor.black.cgColor
+        
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func onSignin(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password)
+        { (user,error) in
+            if user != nil{
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+            else
+            {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+        
+    }
+    
+    @IBAction func onSignup(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground{(success, error) in
+            if success {
+               self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+            else{
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
---
+++ Parstagram/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  Parstagram
+//
+//  Created by Chanyue Hu on 2/25/19.
+//  Copyright © 2019 Chanyue Hu. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
